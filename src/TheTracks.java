722a6113e49ac18ea3077833309dbecb6b2971e6
Benjamin Maus <info@allesblinkt.com>
2012-02-14 14:21:17 +0200

    GC

--- src/TheTracks.java
+++
@@ -1,7 +0,0 @@
-
-public class TheTracks {
-	public static void main(String[] args) {
-		System.out.println("The tracks look very good.");
-		System.out.println("... But my Code is still a bit messy ;)");
-	}
-}
